e107af3cddc8d5f5fb2c70ffadf2614555e2e3f3
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-02-05 14:21:20 -0500

    setup commit

---
+++ src/components/global/footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+const Footer = () => (
+        <>
+            <footer>
+            © {new Date().getFullYear()}, Built with
+              {` `}
+              <a href="https://www.gatsbyjs.org">Gatsby</a>
+            </footer>
+        </>
+    )
+
+
+export default Footer
--- src/components/global/layout.js
+++ src/components/global/layout.js
@@ -8,9 +8,14 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import { ThemeProvider } from 'styled-components'
 
 import Header from "./header"
-import "./layout.css"
+import Footer from "./footer"
+import "../../css/normalize.css"
+import "../../css/fonts.css"
+import "../../css/variables.js"
+
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,21 +30,13 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+      <ThemeProvider theme={ variables }>
+        <>
+          <Header siteTitle={data.site.siteMetadata.title} />
+            <main>{children}</main>
+          <Footer />
+        </>
+      </ThemeProvider>
     </>
   )
 }
